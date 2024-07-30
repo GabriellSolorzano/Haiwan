2b962b47a92c6cf5bde96bc43ae135a8fd8c018c
bochoclero <144356470+bochoclero@users.noreply.github.com>
2024-07-30 11:18:26 -0500

    Add files via upload

--- Haiwan/Vista/app/admin/AnimalFunda.php
+++ Haiwan/Vista/app/admin/AnimalFunda.php
@@ -7,12 +7,12 @@ include('Menu.php');
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Usuarios</h1>
+                    <h1 class="h3 mb-2 text-gray-800">Animal en Fundación</h1>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                        
                         <div class="col-md-12 align-self-end card-header py-3 justify-content-end">
-                            <button class="btn btn-sm bg-gradient-primary offset-md-10 col-md-2" data-toggle="modal" data-target="#exampleModal">Nuevo Usuario</button>
+                            <button class="btn btn-sm btn-primary offset-md-10 col-md-2 " data-toggle="modal" data-target="#exampleModal">Nuevo Usuario</button>
                         </div>
                         <div class="card-body">
                            
@@ -20,56 +20,51 @@ include('Menu.php');
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
-                                            <th>Usuario</th>
-                                            <th>Tipo Documento</th>
-                                            <th>Tipo Usuario</th>
-                                            <th>Identificación</th>
-                                            <th>Nombre</th>
-                                            <th>Apellidos</th>
-                                            <th>Correo</th>
-                                            <th>Contraseña</th>
-                                            <th>Modificar</th>
+                                            <th>Fundacion</th>
+                                            <th>Animal</th>
                                             <th>Eliminar</th>
+                                            <th>Modificar</th>
+
                                         </tr>
                                     </thead>
-                                    <tfoot> 
-                                    </tfoot>
-                                    <?php 
-                                                  include( '../../../Control/Conex.php'); 
-                                                  $cons = $conexion -> query("SELECT *  FROM usuario");
-                                                  while ($row = $cons -> fetch_row()) {
-                                                ?>
-                                            <tr>
-                                        <td><?php echo number_format(''.$row[0].''); ?></td>
-                                        <td><?php echo ''.$row[1].''; ?></td>
-                                        <td><?php echo ''.$row[2].''; ?></td>
-                                        <td><?php echo ''.$row[3].''; ?></td>
-                                        <td><?php echo ''.$row[4].''; ?></td>
-                                        <td><?php echo ''.$row[5].''; ?></td>
-                                        <td><?php echo ''.$row[6].''; ?></td>
-                                        <td><?php echo ''.$row[7].''; ?></td>       
-            
-                   
-                                        <!-- Si el usuario presiona el botòn Modificar ira a el archivo Modificarusua, si presiona eliminar irà a Borrarusua en la Carpeta Control--> 
-                                        <!-- Onclick nos dice a donde se va a dirigir cuando presione el botón-->    
-                                        <td> <center> <button type="submit" class="btn btn-sm bg-gradient-primary"><img src="img/Modificar.png" width="25px"></button> 
-                                        <td> <center><button type="submit" class="btn btn-sm bg-gradient-danger" name="EliminaUsua" onclick="location='../../../Controlador/borrarUsuario.php?id=<?php echo ''.$row[0].'' ?>'"><img src="img/Eliminar.png" width="25px"></button></center></td>
-                                          
-                                                    
-                                    </tr>
-                                    <?php }   ?>
-                                                                  
-                                                          </div>
-                                                      </div>
-                                                  </div>
+                                    <tbody>
+                                        <tr>
+                                            <th>La Perla</th>
+                                            <th>Perro</th>
+                                            <th><center><button class="btn btn-sm btn-danger">Eliminar</center></button></th>
+                                            <th><center><button class="btn btn-sm btn-primary">Modificar</center></button></th>
+                                        </tr>
+                                        <tr>
+                                            <th>PetSon</th>
+                                            <th>Gato</th>
+                                            <th><center><button class="btn btn-sm btn-danger">Eliminar</center></button></th>
+                                            <th><center><button class="btn btn-sm btn-primary">Modificar</center></button></th>
+                                        </tr>
 
-                                              </div>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
                 <!-- /.container-fluid -->
 
             </div>
-            <!-- End of Main , Content -->
+            <!-- End of Main Content -->
 
-            
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2020</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
 
     </div>
     <!-- End of Page Wrapper -->
@@ -93,79 +88,18 @@ include('Menu.php');
           </button>
         </div>
         <div class="modal-body">
-            <form action="../../../controlador/crearUsuario.php">
-              <div class="form-row">
+            <form>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">Nombre de la Fundación:</label>
+                    <input type="text" class="form-control" name="NombreFundacion" placeholder="Digita el nombre" required>
+                  </div>
                 <div class="form-group col-md-6">
-                  <label for="input_nombre">Nombres</label>
-                  <input type="text" class="form-control" name="input_nombre" required>
+                  <label for="inputAddress">Animal:</label>
+                  <input type="number" class="form-control" name="Animal" placeholder="Tipo de Animal">
                 </div>
-                <div class="form-group col-md-6">
-                  <label for="input_apellido">Apellidos</label>
-                  <input type="text" class="form-control" name="input_apellido" required>
                 </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="input_documento">Documento</label>
-                  <input type="number" class="form-control" name="input_documento" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="input_tipoDocumento">Tipo de documento</label>
-                  <select id="inputState" class="form-control">
-                    <?php  
-                        include( '../../../Controlador/conex.php');
-                          # Consultamos a la tabla tipodocu, que es la que tiene los tipos de docuementos en la BD:
-                          $sql = "SELECT * FROM tipodocumento";
-                          $eje = $conexion->query($sql);
-                          # Mostramos a través de un ciclo todas las opciones válidas:
-                          while($row1 = $eje->fetch_row()){
-                            echo '<option value="'.$row1[0].'">'.$row1[1].'</option>';
-                          }
-                        ?>
- </select>
-
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="input_tipoUsuario">Tipo Usuario</label>
-                  <input type="text" class="form-control" name="input_tipoUsuario" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="input_genero">Género</label>
-                  <select name="input_genero" class="form-control" required>
-                  
-                    <option>Masculino</option>
-                    <option>Femenino</option>
-                  </select>
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="input_fechaNacimiento">Fecha de nacimiento</label>
-                  <input type="date" class="form-control" name="input_fechaNacimiento" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="input_direccion">Dirección</label>
-                  <input type="address" class="form-control" name="input_direccion" required>
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-12">
-                  <label for="input_telefono">Teléfono</label>
-                  <input type="tel" class="form-control" name="input_telefono" required>
-                </div>
-              </div>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label for="input_email">Correo</label>
-                  <input type="email" class="form-control" name="input_email" required>
-                </div>
-                <div class="form-group col-md-6">
-                  <label for="input_contrasena">Contraseña</label>
-                  <input type="password" class="form-control" name="input_contrasena" required>
-                </div>
-              </div>
+                
               <center> <button type="submit" class="btn btn-primary">Guardar</button></center> 
               </form>
         </div>
@@ -173,21 +107,8 @@ include('Menu.php');
       </div>
     </div>
   </div>
-  </div>
-
-  </div>
-    <!-- Footer -->
-    <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
-        </div>
-        <!-- End of Content Wrapper -->
+  
+    
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
